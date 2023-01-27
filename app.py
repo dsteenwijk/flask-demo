318e75421e1d999862a983d50ccaf70d6dec4ce4
Dennis Steenwijk <steed02@macos-c02fnasgmd6m-steed02.home>
2023-01-27 13:44:36 +0100

    Changed text again

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route('/')
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
-    return 'Hello Dutch Tax Office!'
+    return 'Hello Dutch Tax Office! v2'
  
 # main driver function
 if __name__ == '__main__':
